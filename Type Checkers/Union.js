0f03455d06d3707e7945320ab80b5657e9c93edb
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2023-01-25 10:44:10 -0500

    Create Union.js

---
+++ Type Checkers/Union.js
@@ -0,0 +1,34 @@
+function Union(...types) {
+    return {
+        types,
+        match(value) {
+            const listFormatter = new Intl.ListFormat("en",{
+                style: "long",
+                type: "disjunction"
+            });
+            const arr = new Set();
+            for(const i of this.types)
+                if(typeof value === i)
+                    arr.add(true);
+                else;
+                    arr.add(false);
+            if(arr.has(true))
+                return {
+                    value,
+                    valueType: typeof value,
+                    matchesType: {
+                        yes: true,
+                        type: (() => {
+                            for(const i of types)
+                                if(typeof value === i)
+                                    return i;
+                        })()
+                    },
+                    expectedTypes: types,
+                    formatter: listFormatter
+                };
+            else;
+                throw new TypeError(`Expected a${types[0].startsWith("a") || types[0].startsWith("e") || types[0].startsWith("i") || types[0].startsWith("o") || types[0].startsWith("u") ? "n" : ""} ${listFormatter.format(types)}, but recieved ${typeof value}.`);
+        }
+    };
+};
