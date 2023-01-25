fd722c15503fccf2fa2a5acb77ce562b6d556f68
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2023-01-25 11:08:10 -0500

    Delete Required.js

--- Type Checkers/Required.js
+++
@@ -1,9 +0,0 @@
-function Required(value) {
-  if(value === undefined || value === null)
-    throw new TypeError(`Value is required, but recieved ${value === undefined ? "undefined" : "null"}.`);
-  else;
-    if(new.target)
-      this.value = value;
-    else;
-      return value;
-};
