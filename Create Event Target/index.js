3f176c6865f0acbda3223b83525fec7bb1aac3d4
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2023-01-31 18:54:40 -0500

    Update index.js

--- Create Event Target/index.js
+++ Create Event Target/index.js
@@ -6,6 +6,6 @@ const cet = function createEventTarget(options = {}) {
   if (Array.isArray(options))
     throw new TypeError("Options for function must be an object, not an array.");
   else if (typeof options !== "object")
-    throw new TypeError(`Options for function must be an object, not type of ${Array.isArray(options) ? "array" : typeof options}`);
+    throw new TypeError(`Options for function must be an object, not type of ${Array.isArray(options) ? "array" : typeof options}.`);
   return et;
 };
