82c9e5f3a47448aaa3f5e8663f8f94caa9b16fc6
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2023-02-19 11:04:34 -0500

    Update index.js
    Minimized code.
    Added script for voice selection.

--- Simple Text to Speech/index.js
+++ Simple Text to Speech/index.js
@@ -1,7 +1,6 @@
 try {
   console.clear();
   const text = document.querySelector("textarea#text");
-  const _lang = document.getElementById("_lang");
   const speak = document.getElementById("_speak");
   const pitch = document.getElementById("pitch");
   const rate = document.getElementById("rate");
@@ -32,57 +31,49 @@ try {
     console.log("Hit a boundary on", textToRead);
     console.log("Event:", e);
   };
-  text.addEventListener("change", (e) => {
-    speechSynthesis.cancel();
-    textToRead.text = text.value;
-    textToRead.pitch = Number(pitch.value);
-    textToRead.lang = _lang.value;
-    textToRead.rate = Number(rate.value);
-    textToRead.volume = Number(volume.value);
-    console.log("Event:", e);
-    console.log("Speech Synthesis Utterance:", textToRead);
-  });
-  for (const element of document.querySelectorAll("input"))
-    element.addEventListener("change", (e) => {
-      speechSynthesis.cancel();
-      textToRead.pitch = pitch.value;
-      textToRead.lang = _lang.value;
-      textToRead.rate = rate.value;
-      textToRead.text = text.value;
-      textToRead.volume = Number(volume.value);
-      console.log("Event:", e);
-      console.log("Speech Synthesis Utterance:", textToRead);
-    });
+  function onvoiceschanged() {
+    const voices = speechSynthesis.getVoices();
+    const voices_l = document.getElementById("voices_l");
+    for (const voice of voices) {
+      const option = document.createElement("option");
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.setAttribute("data-name", voice.name);
+      option.setAttribute("data-lang", voice.lang);
+      option.setAttribute(
+        "data-json",
+        JSON.stringify({
+          voiceURI: voice.voiceURI,
+          name: voice.name,
+          lang: voice.lang,
+          localService: voice.localService,
+          default: voice.default
+        })
+      );
+      voices_l.appendChild(option);
+    }
+  }
+  onvoiceschanged();
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = onvoiceschanged;
+  }
   speak.addEventListener("click", (e) => {
     speechSynthesis.cancel();
+    textToRead.pitch = pitch.value;
+    textToRead.rate = rate.value;
+    textToRead.text = text.value;
+    textToRead.volume = Number(volume.value);
+    const voices_l = document.querySelector("select");
+    for (const voice of speechSynthesis.getVoices())
+      if (voice.name === voices_l.selectedOptions[0].getAttribute("data-name"))
+        textToRead.voice = voice;
+    textToRead.lang = textToRead.voice.lang;
     speechSynthesis.speak(textToRead);
+    console.log("Speech Synthesis Utterance:", textToRead);
     console.log("Event:", e);
   });
-  if (
-    window.confirm(
-      "Do you want to view the voices that you have currently have installed in your device? "
-    )
-  ) {
-    const formatter = new Intl.ListFormat("all", {
-      style: "long",
-      type: "conjunction"
-    });
-    const voices = speechSynthesis.getVoices();
-    const arr = Array(voices.length);
-    for (const i in voices) {
-      const voice = voices[i];
-      arr[i] = `${voice.name} (${voice.lang})`;
-    }
-    window.alert(`Voices: ${formatter.format(arr)}`);
-    window.alert(`Note: if you haven't seen any voices in the previous message, please go to "https://github.com/SuperIden3/My-Snippets/blob/main/Simple%20Text%20to%20Speech" and paste the code correctly in a code editor other than CodeSandBox or Replit for it to work.\nCodeSandBox and Replit was tested on and voices didn't get recieved into the message, so I would not recommend pasting the code in CodeSandBox.\nUse StackBlitz or another code editor you know you think it would work on.\nAlso, only use "index.html" and "index.js" to paste in the browser, not any other file.`);
-    if(window.confirm("Open link? "))
-      if(window.open)
-        window.open("https://github.com/SuperIden3/My-Snippets/blob/main/Simple%20Text%20to%20Speech");
-      else {
-        window.alert("Could not open link, so it will be copied to your clipboard.");
-        window.copy("https://github.com/SuperIden3/My-Snippets/blob/main/Simple%20Text%20to%20Speech");
-      }
-  }
 } catch (e) {
   console.error(e);
 }
