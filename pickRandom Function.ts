4a93c0377eb7c65cce95e5b0c3b5461b7faf2d4a
The Annoying One <95597246+SuperIden3@users.noreply.github.com>
2023-03-08 10:38:12 -0500

    Delete pickRandom Function.ts

--- pickRandom Function.ts
+++
@@ -1,9 +0,0 @@
-/*
-  visit https://www.delftstack.com/howto/javascript/javascript-pick-random-from-array for more info.
-  Credit to that person entirely.
-  Website not mine btw.
-*/
-var pickRandom = (array: any[]): any => {
-  let random: number = Math.random() * array.length | 0;
-  return array[random];
-}
