290cedee65a48caddac1e28eaf36454899f02024
The Annoying One <95597246+SuperIden3@users.noreply.github.com>
2023-03-12 15:37:44 -0400

    Update Reverse a Sentence.js
    Fixed indentation, added a default export, and changed its declaration syntax to a regular function.

--- Reverse a Sentence.js
+++ Reverse a Sentence.js
@@ -2,23 +2,17 @@
  * Reverse a sentence.
  * @param {string} string The sentence to reverse.
  */
-var reverseSentence = string => {
-  let punctuation = string.charAt(string.length - 1)
-	string = string.replace(punctuation, "")
-	let arr = string.split(" ")
-	arr.reverse()
-	if(arr[arr.length - 1].charAt(0) === arr[arr.length - 1].charAt(0).toUpperCase()) {
-	  arr[arr.length - 1] = arr[arr.length - 1].replace(arr[arr.length - 1].charAt(0),arr[arr.length - 1].charAt(0).toLowerCase())
-	} else {
-	  arr[arr.length - 1] = arr[arr.length - 1]
-	}
-	arr[0] = arr[0].replace(arr[0].charAt(0),arr[0].charAt(0).toUpperCase())
-	arr[arr.length - 1] += punctuation
-	return arr.join(" ")
+function reverseSentence(string) {
+  let punctuation = string.charAt(string.length - 1);
+  string = string.replace(punctuation, "");
+  let arr = string.split(" ");
+  arr.reverse();
+  if(arr[arr.length - 1].charAt(0) === arr[arr.length - 1].charAt(0).toUpperCase())
+    arr[arr.length - 1] = arr[arr.length - 1].replace(arr[arr.length - 1].charAt(0),arr[arr.length - 1].charAt(0).toLowerCase());
+  else
+    arr[arr.length - 1] = arr[arr.length - 1];
+  arr[0] = arr[0].replace(arr[0].charAt(0),arr[0].charAt(0).toUpperCase());
+  arr[arr.length - 1] += punctuation;
+  return arr.join(" ");
 }
-// Example: "Hello there!" Becomes "There hello!"
-// Abbreviation:
-var rs = reverseSentence
-
-// Sentence must require punctuation, or else the last letter of the sentence will be moved to be the first.
-// Note: Special names, like "GitHub", used at the beginning returns "gitHub" at the end.
+export default reverseSentence;
